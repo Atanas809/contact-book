6464f7133a9e647236f028b19c30d98c2c5b62fd
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-19 10:20:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-aaaaa
-
-def print_hi(name):
+i(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
